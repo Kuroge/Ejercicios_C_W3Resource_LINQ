97198219121fb48f4f8ba88e7f9ec9676ea21c96
Adrián García Riera <i7610@softgal.com>
2018-02-02 12:48:49 +0100

    02/02/18

---
+++ Exercise13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int size;
+            Console.WriteLine("Indica el tamaño del array");
+            size = Convert.ToInt32(Console.ReadLine());
+            string[] array = new string[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Introduce el valor {i+1}");
+                array[i] = Console.ReadLine();
+            }
+
+            string newstring = string.Join(", ", array);
+            Console.WriteLine(newstring);
+            Console.ReadKey();
+        }
+    }
+}
